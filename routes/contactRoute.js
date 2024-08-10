84266ee873e8a36c9984ef909d1f0ad4bafb5aa7
anirudh <anirudh@kindtech.co.in>
2024-08-10 18:53:32 +0530

    chaining all those which were having same routes

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,15 +3,8 @@ const router = express.Router();
 const { getContact, getSpecificContact, createContact, updateContact, deleteContact } = require('../controllers/contactControllers')
 
 // here we have passed all the functions to controllers folder
-router.route('/').get(getContact)
+router.route('/').get(getContact).post(updateContact)
 
-router.route('/:id').get(getSpecificContact)
-
-router.route('/:id').put(createContact)
-
-router.route('/').post(updateContact)
-
-router.route('/:id').delete(deleteContact)
-
+router.route('/:id').get(getSpecificContact).put(createContact).delete(deleteContact)
 
 module.exports = router;
