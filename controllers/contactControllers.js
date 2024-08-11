ddf1000c750e8f23f1640b7af805b2d690f30ddf
anirudh <anirudh@kindtech.co.in>
2024-08-11 16:21:44 +0530

    we want to connect it with mongoDB so we havw to make all fns async

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -3,28 +3,28 @@ const colors = require('colors')
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = async (req, res) => {
     res.status(200).json({ message: 'get all contacts' });
 }
 
 //@desc get a specific contact
 //@route  Get /api/contatcts/:id
 //@access public
-const getSpecificContact = (req, res) => {
+const getSpecificContact = async (req, res) => {
     res.status(200).json({ message: `create specific contact of ${req.params.id}` });
 }
 
 //@desc put a new specific contact
 //@route Put /api/contatcts/:id
 //@access public
-const createContact = (req, res) => {
+const createContact = async (req, res) => {
     res.status(201).json({ message: `put specific contact of ${req.params.id}` });
 }
 
 //@desc update contact
 //@route Post /api/contatcts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = async (req, res) => {
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         res.status(400);
@@ -37,8 +37,8 @@ const updateContact = (req, res) => {
 //@desc delete a specific contact
 //@route Delete /api/contatcts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = async (req, res) => {
     res.status(200).json({ message: `delete specific contact of ${req.params.id}` });
 }
 
-module.exports = { getContact, getSpecificContact, createContact, updateContact,deleteContact }
+module.exports = { getContact, getSpecificContact, createContact, updateContact, deleteContact }
