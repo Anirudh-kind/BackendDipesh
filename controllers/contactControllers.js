d78819da4fb087799273313be221e2f815a07cc8
anirudh <anirudh@kindtech.co.in>
2024-08-14 08:39:13 +0530

    made schema and build apis

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,44 +1,62 @@
-const colors = require('colors')
 const asyncHandler = require('express-async-handler')// just wrap the function around it and no need to use try-catch block
+const Contact = require('../models/contactModel')
 
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access public
 const getContact = async (req, res) => {
-    res.status(200).json({ message: 'get all contacts' });
+    const contacts = await Contact.find()
+    res.status(200).json(contacts);
 }
 
 //@desc get a specific contact
 //@route  Get /api/contatcts/:id
 //@access public
 const getSpecificContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `create specific contact of ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404);
+        throw new Error(`contact not find`.red)// getting TypeError: Cannot read properties of undefined (reading 'VALIDATION_ERROR') insted of this
+    }
+    res.status(200).json(contact);
 })
 
-//@desc put a new specific contact
+//@desc  update contact
 //@route Put /api/contatcts/:id
 //@access public
-const createContact = asyncHandler(async (req, res) => {
-    res.status(201).json({ message: `put specific contact of ${req.params.id}` });
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findOneAndUpdate(req.params.id, req.body, { upsert: true, new: true }) // throwing error here
+    res.status(200).json(contact);
 })
 
-//@desc update contact
-//@route Post /api/contatcts/:id
+//@desc create a new contact
+//@route Post /api/contatcts/
 //@access public
-const updateContact = asyncHandler(async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         res.status(400);
         throw new Error("all fields are mandatory".bgRed)
     }
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    })
+    res.status(200).json(contact);
     console.log(`req.body is :${JSON.stringify(req.body)}`.cyan)
-    res.status(200).json({ message: `update contacts ${req.params.id}` });
 })
 
 //@desc delete a specific contact
 //@route Delete /api/contatcts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete specific contact of ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404);
+        throw new Error(`contact not find`.red)// getting TypeError: Cannot read properties of undefined (reading 'VALIDATION_ERROR') insted of this
+    }
+    await contact.remove()
+    res.status(200).json(contact);
 })
 module.exports = { getContact, getSpecificContact, createContact, updateContact, deleteContact }
